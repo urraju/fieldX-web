3a85af455d0f970d2724b49358cc4a49bdb36a15
urraju <riadhasan680@gmail.com>
2024-02-02 17:31:17 +0600

    setup link

--- src/AuthContext/AuthContext.jsx
+++ src/AuthContext/AuthContext.jsx
@@ -41,10 +41,10 @@ const AuthContext = ({children}) => {
             setLoading(false)
 
             if(currentUser) {
-                axios.post('http://localhost:3000/jwt',loggedUser, {withCredentials : true})
+                axios.post('https://field-x-server.vercel.app/jwt',loggedUser, {withCredentials : true})
                 .then(res => console.log('token respons', res.data))
             }else{
-                axios.post('http://localhost:3000/logout',loggedUser, {withCredentials : true})
+                axios.post('https://field-x-server.vercel.app/logout',loggedUser, {withCredentials : true})
                 .then(res => {
                     console.log('success data', res.data);
                 })
